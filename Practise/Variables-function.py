bc763359ad27800d0cbdabaf8c6a980534f7f3fb
jaykishan kargathara <jaykishankargathara@outlook.com>
2019-10-12 16:15:56 +0530

    Code added.

---
+++ Practise/Variables-function.py
@@ -0,0 +1,8 @@
+a='mast'
+
+def firstDef():
+    a='cool'
+    print ("cool " + a)
+firstDef()
+
+print ("cool " + a)
---
+++ Practise/Variables-global.py
@@ -0,0 +1,8 @@
+Name='jk'
+
+def secondDef():
+    global Name
+    Name = "jaykay"
+    print ("name is " + Name)
+secondDef()
+print ("name is " + Name)
