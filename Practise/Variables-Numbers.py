f49e8199480016b21c615820269f25fefe129505
jaykishan kargathara <jaykishankargathara@outlook.com>
2019-10-12 16:41:51 +0530

    Code added.

---
+++ Practise/Variables-Numbers.py
@@ -0,0 +1,7 @@
+JK = 25
+jk = 20
+print (JK, jk)
+print (jk, JK)
+print (jk+JK)
+print (jk)
+print (JK)
---
+++ Practise/While-loops.py
@@ -0,0 +1,31 @@
+name = ["jk", "jay kay", "Jaykishan"]
+for x in name:
+    print (x)
+
+name = ['jk', 'jay kay', 'jaykishan']
+for x in 'jay kay':
+    print (x)
+
+name = ['jk', 'jay kay', 'jaykishan']
+for x in name:
+    print (x)
+    if x == 'jay kay':
+        break
+
+name = ['jk', 'jay kay', 'jaykishan']
+for x in name:
+    if x == 'jay kay':
+        break
+    print (x)
+
+name = ['jk', 'jay kay', 'jaykishan']
+for x in name:
+    if x == 'jay kay':
+        continue
+    print (x)
+
+for x in range(5):
+    print (x)
+
+for x in range (1, 5):
+    print (x)
